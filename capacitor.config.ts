5234e049fc88b3a31b695289c7dc4a351e0dcc45
Pavan Dange <pavandange98@gmail.com>
2025-05-19 23:20:17 +0530

    IT is now abble to store the message

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.46827b0d769c4b8491c875474c8c6b59',
-  appName: 'Todo List App',
-  webDir: 'dist',
-  server: {
-    url: 'https://46827b0d-769c-4b84-91c8-75474c8c6b59.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
-    }
-  }
+  appId: 'com.example.app',
+  appName: 'vite_react_shadcn_ts',
+  webDir: 'dist'
 };
 
 export default config;
